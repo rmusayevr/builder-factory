55b951dd4f498b0c1904dcc4638371ce89e59265
Rashad Musayev <rmusayevr19@gmail.com>
2024-07-10 12:40:01 +0400

    fix: lint

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,6 @@ from unittest.mock import patch
 
 import pytest
 
-from src.pytemplate.domain.models import Burger
 from src.pytemplate.entrypoints.cli.main import main
 
 
@@ -12,7 +11,7 @@ from src.pytemplate.entrypoints.cli.main import main
         ("cheese", "Burger with Sesame Seed Bun, Beef Patty, Ketchup, no toppings."),
         ("vegan", "Burger with Whole Wheat Bun, Black Bean Patty, Vegan Mayo, Lettuce, Tomato."),
         ("chicken", "Burger with Brioche Bun, Chicken Patty, Mayo, Pickles."),
-        ("invalid", "Invalid choice!")
+        ("invalid", "Invalid choice!"),
     ],
 )
 def test_main(choice, expected_result):
